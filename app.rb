fb0a10d61946ce14aa99aab7de34927a05dd0556
Michal Klesinski <michkles@Michals-MacBook-Pro.local>
2016-11-28 16:00:23 +0000

    testing app.rb

--- app.rb
+++ app.rb
@@ -8,6 +8,35 @@ class Dataserver < Sinatra::Base
     'Hello Dataserver!'
   end
 
+  post '/set' do
+
+    #value = params.values
+    #p session[key] = value
+    #michal
+    #{"name"=>"what"}
+     #keys
+    session[:value] = params.values
+    session[:name] = params[:name]#what
+    session[:key] = params.keys
+  end
+
+ get '/get' do
+
+   p key = params[:key]
+
+  # p params #{"key"=>"name"}
+
+
+   #http://localhost:4000/get?key=name
+   #http://localhost:9393/whatsup?name=bruce&surname=wayne
+   #The application can handle these params in the controller as follows:
+
+  # get '/whatsup' do
+  #   "Whats up " + params[:name] + " " + params[:surname]"
+  # end
+   #  visit 'http://localhost:4000/set?name=garfied'
+   # visit 'http://localhost:4000/get?key=name'
+end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
